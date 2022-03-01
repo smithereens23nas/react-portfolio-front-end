2b05b58315c8d84a1c3b201be09ee987e5283cfb
smiethereens23nas <nicholas23smith@gmail.com>
2022-03-01 17:49:00 -0500

    fixed bugs

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, getAboutData } from "react";
 
 function About(props) {
   // create state to hold about data
   const [about, setAbout] = useState(null);
 
   // create function to make api call
-  const getAboutData = async () => {
+useEffect(() => {  const getAboutData = async () => {
     // make api call and get response
     const response = await fetch(props.URL + "about");
     // turn response into javascript object
@@ -15,7 +15,7 @@ function About(props) {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getAboutData(), []);
+  }, getAboutData(), []);
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => (
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, getProjectsData } from "react";
 
 function Projects(props) {
   // create state to hold projects
   const [projects, setProjects] = useState(null);
 
   //create function to make api call
-  const getProjectsData = async () => {
+  useEffect(() => {const getProjectsData = async () => {
     //make api call and get response
     const response = await fetch(props.URL + "projects");
     // turn response into javascript object
@@ -14,15 +14,15 @@ function Projects(props) {
     setProjects(data);
   };
 
-  // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getProjectsData(), []);
+// make an initial call for the data inside a useEffect, so it only happens once on component load
+}, getProjectsData(), []);
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => {
     return projects.map((project) => (
       <div>
         <h1>{project.name}</h1>
-        <img src={project.image} />
+        <img src={project.image} alt='' />
         <a href={project.git}>
           <button>Github</button>
         </a>

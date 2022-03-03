5116ad340704f79e76158af692ec292358a3a3de
smiethereens23nas <nicholas23smith@gmail.com>
2022-03-03 15:34:24 -0500

    made a home frontend

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route exact path="/home" element={<Home URL={URL} />} />
+        <Route exact path="/" element={<Home URL={URL} />} />
         <Route path="/projects" element={<Projects URL={URL} />} />
         <Route path="/about" element={<About URL={URL} />} />
       </Routes>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,11 @@ import { useState, useEffect } from "react";
 function Home(props) {
   // create state to hold home
   const [home, setHome] = useState(null);
-
+  console.log(props);
   //create function to make api call
   const getHomeData = async () => {
     //make api call and get response
-    const response = await fetch(props.URL + 'home');
+    const response = await fetch(props.URL);
     // turn response into javascript object
     const data = await response.json();
     // set the home state to the data
@@ -15,16 +15,18 @@ function Home(props) {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getHomeData());
+  useEffect(() => getHomeData(), []);
 
   // define a function that will return the JSX needed once we get the data
-  const loaded = () => (
-    <div>
-      <h2>{home.name}</h2>
-      <h3>{home.bio}</h3>
-      <p>{home.email}</p>
-    </div>
-  );
+  const loaded = () => {
+    return home.map((element, index) => (
+      <div key={index}>
+        <h1>{element.name}</h1>
+        <p>{element.bio}</p>
+        <h4>{element.email}</h4>
+      </div>
+    ));
+  };
 
   return home ? loaded() : <h1>Loading...</h1>;
 }

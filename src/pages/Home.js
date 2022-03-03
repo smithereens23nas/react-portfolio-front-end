5df953d986b01fb17ff8547322abf5eef0465713
smiethereens23nas <nicholas23smith@gmail.com>
2022-03-03 13:35:09 -0500

    added home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
+
 function Home(props) {
-    return <h1>Home</h1>;
-  }
-  
-  export default Home;
+  // create state to hold home
+  const [home, setHome] = useState(null);
+
+  //create function to make api call
+  const getHomeData = async () => {
+    //make api call and get response
+    const response = await fetch(props.URL + 'home');
+    // turn response into javascript object
+    const data = await response.json();
+    // set the home state to the data
+    setHome(data);
+  };
+
+  // make an initial call for the data inside a useEffect, so it only happens once on component load
+  useEffect(() => getHomeData());
+
+  // define a function that will return the JSX needed once we get the data
+  const loaded = () => (
+    <div>
+      <h2>{home.name}</h2>
+      <h3>{home.bio}</h3>
+      <p>{home.email}</p>
+    </div>
+  );
+
+  return home ? loaded() : <h1>Loading...</h1>;
+}
+
+export default Home;
